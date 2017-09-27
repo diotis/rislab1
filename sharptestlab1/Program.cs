ec08c3976f896f42e04645642835e2d342355c28
diotis <dio.user@mail.ru>
2017-09-27 16:34:15 +0300

    end

--- sharptestlab1/Program.cs
+++ sharptestlab1/Program.cs
@@ -11,9 +11,10 @@ namespace sharptestlab1
     [Serializable]
     class Program
     {
-        private String region;
-        private String tkind;
-        private long quant;
+        private String marka;
+        private String model;
+        private long year;
+        private bool err = false;
 
         static List<Program> list;
         static bool newList;
@@ -21,19 +22,19 @@ namespace sharptestlab1
 
         private void data(String r, String t, long q)
         {
-            this.region = r;
-            this.tkind = t;
-            this.quant = q;
+            this.marka = r;
+            this.model = t;
+            this.year = q;
         }
 
         private Program(String r,String t,long q) {
-            this.region = r;
-            this.tkind = t;
-            this.quant = q;
+            this.marka = r;
+            this.model = t;
+            this.year = q;
         }
         public string show()
         {
-            return this.region+" "+this.tkind+" "+this.quant;
+            return this.marka+" "+this.model+" "+this.year;
         }
 
         public static void Serialize()
@@ -77,107 +78,195 @@ namespace sharptestlab1
             newList = false;
             return list;
         }
+
+        Program vvod()
+        {
+            Program obj = new Program();
+            try
+            {
+                String marka = Console.ReadLine();
+                if (marka.Length == 0)
+                {
+                    throw new Exception("Ничего не было введено!");
+                }
+                String model = Console.ReadLine();
+                if (model.Length == 0)
+                {
+                    throw new Exception("Ничего не было введено!");
+                }
+                long year = System.Int64.Parse(Console.ReadLine());
+                obj.data(marka,model,year);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+                obj.err = true;
+                Console.ReadLine();
+            }
+            return obj;
+        }
+
         static void Main(string[] args)
         {
-            String choice = " ";
-            newList = true;
-            //открытие файла с данными на запись
-            list = new List<Program>();
-            while (choice != "7")
+            try
             {
-                if (newList == true)
+                String choice = " ";
+                newList = true;
+                //открытие файла с данными на запись
+                list = new List<Program>();
+                while (choice != "7")
                 {
-                    list = Deserealize();
+                    if (newList == true)
+                    {
+                        list = Deserealize();
+                    }
+                    FileStream fstr = new FileStream("d:\\files\\ris\\file.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+                    Console.WriteLine("Выберите пункт:\n 1 - Ввести новую запись\n 2 - Посмотреть список автомобилей\n 3 - Посмотреть автомобили конкретной марки\n"
+                        + " 4 - Удалить запись\n 5 - Редактировать запись\n 6 - Сортировать по модели\n 7 - Выход");
+                    choice = Console.ReadLine();
+
+                    switch (choice)
+                    {
+                        case "1":
+                            {
+                                Console.WriteLine("Введите марку автомобиля, модель и год выпуска:");
+
+                                Program obj = new Program();
+
+                                obj = obj.vvod();
+                                if (obj.err == false)
+                                {
+                                    fstr.Seek(0, SeekOrigin.End);
+                                    BinaryFormatter bf = new BinaryFormatter();
+                                    bf.Serialize(fstr, obj);
+                                    list.Add(obj);
+                                }
+                                break;
+                            }
+                        case "2":
+                            {
+                                Console.WriteLine("Список:");
+                                int i = 0;
+                                if (list.Count == 0)
+                                {
+                                    Console.WriteLine("Список пуст");
+                                }
+                                else
+                                    foreach (var ob in list)
+                                    {
+                                        i++;
+                                        Console.WriteLine(i + ") " + ob.show());
+                                    }
+                                Console.ReadLine();
+                                break;
+                            }
+                        case "3":
+                            {
+                                Console.WriteLine("Введите марку автомобиля для поиска");
+                                string r = Console.ReadLine();
+                                if (r.Length != 0)
+                                {
+                                    Console.WriteLine();
+                                    for (int i = 0; i < list.Count; i++)
+                                    {
+                                        if (list.ElementAt(i).marka.Equals(r))
+                                            Console.WriteLine(list.ElementAt(i).show());
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Ошибка ввода!");
+                                }
+                                Console.ReadLine();
+                                break;
+                            }
+                        case "4":
+                            {
+                                Console.WriteLine("Введите марку автомобиля, модель и год выпуска:");
+                                try
+                                {
+                                    int num = Convert.ToInt32(Console.ReadLine());
+
+                                    if (num <= 0)
+                                    {
+                                        throw new Exception("Введенное число - отрицательно");
+                                    }
+                                    if (num > list.Count)
+                                    {
+                                        throw new Exception("Введенное число больше размера списка");
+                                    }
+                                    num--;
+                                    if (num > 0 && num < list.Count)
+                                    {
+                                        list.RemoveAt(num);
+                                        fstr.Close();
+                                        Serialize();
+                                        newList = true;
+                                    }
+                                    Console.WriteLine("Удалено!");
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine("Ошибка: " + ex.Message);
+                                }
+                                Console.ReadLine();
+                                break;
+                            }
+                        case "5":
+                            {
+                                Console.WriteLine("Введите номер записи для редактирования:");
+                                try
+                                {
+                                    int num = Convert.ToInt32(Console.ReadLine());
+
+                                    if (num <= 0)
+                                    {
+                                        throw new Exception("Введенное число - отрицательно");
+                                    }
+                                    if (num > list.Count)
+                                    {
+                                        throw new Exception("Введенное число больше размера списка");
+                                    }
+                                    num--;
+                                    if (num > 0 && num < list.Count)
+                                    {
+                                        Console.WriteLine("Редактируемая запись: " + list.ElementAt(num).show());
+                                        Console.WriteLine("Введите марку автомобиля, модель и год выпуска:");
+                                        Program obj = new Program();
+                                        obj = obj.vvod();
+                                        if (obj.err == false)
+                                        {
+                                            list.ElementAt(num).data(obj.marka, obj.model, obj.year);
+                                            fstr.Close();
+                                            Serialize();
+                                            newList = true;
+                                        }
+                                    }
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine("Ошибка: " + ex.Message);
+                                }
+                                Console.ReadLine();
+                                break;
+                            }
+                        case "6":
+                            {
+                                list.Sort(delegate (Program ob1, Program ob2)
+                                { return ob1.marka.CompareTo(ob2.marka); });
+                                Console.WriteLine("Отсортировано!");
+                                Console.ReadLine();
+                                break;
+                            }
+                    }
+                    Console.Clear();
+                    fstr.Close();
                 }
-                FileStream fstr = new FileStream("d:\\files\\ris\\file.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
-               
-                Console.WriteLine("Vyberite punkt:\n 1 - Vvesti novuju zapis\n 2 - Posmotret' posadki derevjev v rajonax\n 3 - Posmotret konkretnij rajon\n"
-                    + " 4 - Udalit zapis\n 5 - Redaktirovat' zapis\n 6 - Sortirovat' po rajonu\n 7 - Exit");
-                choice = Console.ReadLine();
-
-                switch (choice)
-                {
-                    case "1":
-                        {
-                            Console.WriteLine("Vvedite rajon, porodu dereva i kolichestvo:");
-                            String region = Console.ReadLine();
-                            String tkind = Console.ReadLine();
-                            long quant = System.Int64.Parse(Console.ReadLine());
-                            Program obj = new Program(region, tkind, quant);
-                            fstr.Seek(0, SeekOrigin.End);
-
-                            BinaryFormatter bf = new BinaryFormatter();
-                            bf.Serialize(fstr, obj);
-                            list.Add(obj);
-                            break;
-                        }
-                    case "2":
-                        {
-                            Console.WriteLine("Список:");
-                            int i = 0;
-                            foreach (var ob in list)
-                            {
-                                i++;
-                                Console.WriteLine(i+") "+ob.show());
-                            }
-                            Console.ReadLine();
-                            break;
-                        }
-                    case "3":
-                        {
-                            Console.WriteLine("Введите название раойна для поиска");
-                            string r = Console.ReadLine();
-                            Console.WriteLine();
-                            for (int i = 0; i < list.Count; i++)
-                            {
-                                if(list.ElementAt(i).region.Equals(r))
-                                Console.WriteLine(list.ElementAt(i).show());
-                            }
-                            Console.ReadLine();
-                            break;
-                        }
-                    case "4":
-                        {
-                            Console.WriteLine("Введите номер записи на удаление:");
-                            int num = Convert.ToInt32(Console.ReadLine());
-                            if (num>0&&num<list.Count)
-                            list.RemoveAt(num-1);
-                            fstr.Close();
-                            Serialize();
-                            newList = true;
-                            break;
-                        }
-                    case "5":
-                        {
-                            Console.WriteLine("Введите номер записи для редактирования:");
-                            int num = Convert.ToInt32(Console.ReadLine());
-                            num--;
-                            if (num > 0 && num < list.Count)
-                            {
-                                Console.WriteLine("Редактируемая запись: "+list.ElementAt(num).show());
-                                Console.WriteLine("Vvedite rajon, porodu dereva i kolichestvo:");
-                                String region = Console.ReadLine();
-                                String tkind = Console.ReadLine();
-                                long quant = System.Int64.Parse(Console.ReadLine());
-                                list.ElementAt(num).data(region,tkind,quant);
-                                fstr.Close();
-                                Serialize();
-                                newList = true;
-                            }
-                            Console.ReadLine();
-                            break;
-                        }
-                    case "6":
-                        {
-                            list.Sort(delegate (Program us1, Program us2)
-                            { return us1.region.CompareTo(us2.region); });
-
-                            Console.ReadLine();
-                            break;
-                        }
-                }
-                Console.Clear();
-                fstr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
             }
         }
     }
